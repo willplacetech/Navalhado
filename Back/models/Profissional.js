32770b67215863c83f5448dae7030f496bfab512
willplacetech <wilquer.figueiredo@placetech.com.br>
2026-08-17 16:02:41 -0300

    teste

--- Back/models/Profissional.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const profissionalSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  especialidade: { type: String, required: true },
-  ativo: { type: Boolean, default: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Profissional', profissionalSchema);
--- Front/build/service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'salao-v1';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
